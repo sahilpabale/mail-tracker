ea274370d5fc9178dd779fd0135c3ecd90e171ed
sahilpabale <dev.sahilpabale@gmail.com>
2021-02-15 14:23:37 +0530

    2nd commit

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,28 @@
 import express from "express";
 import sendMail from "./apps/sendMail";
 
+if (process.env.NODE_ENV == "development") {
+  require("dotenv").config();
+}
+
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
   res.send("hello");
+});
+
+app.get("/send", async (req, res) => {
+  res.send("sent!");
   await sendMail("dev.sahilpabale@gmail.com");
 });
 
 app.get("/track", (req, res) => {
   // res.send("email opened");
-  console.log("email opened");
+  console.log("Email Opened at :" + Date());
 });
 
-app.listen(3000, () => {
-  console.log(`http://localhost:3000`);
+app.listen(PORT, () => {
+  console.log(`server up on port ${PORT}`);
 });
